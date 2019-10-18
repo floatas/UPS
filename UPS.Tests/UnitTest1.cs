670981d7dc56270ee3697a70f8c8cc60f701638f
Šarūnas <floatas@outlook.com>
2019-10-18 19:44:35 +0300

    get actualpaths

--- UPS.Tests/UnitTest1.cs
+++ UPS.Tests/UnitTest1.cs
@@ -13,10 +13,16 @@ namespace UPS.Tests
             var statuses = (new ProjectStructurizer()).GetProjectStatuses(solutionPath);
 
             Assert.Equal(4, statuses.Count());
+
             Assert.Contains(statuses, s => s.ProjectName.Equals("StructurlFailure.csproj"));
             Assert.Contains(statuses, s => s.ProjectName.Equals("MusicLibrary.csproj"));
             Assert.Contains(statuses, s => s.ProjectName.Equals("MusicianLibrary.csproj"));
             Assert.Contains(statuses, s => s.ProjectName.Equals("DatabaseConnection.csproj"));
+
+            Assert.Contains(statuses, s => s.ActualPath.Equals(@"C:\Users\sarun\source\repos\UPS\TestProject\StructurlFailure\StructurlFailure\StructurlFailure.csproj"));
+            Assert.Contains(statuses, s => s.ActualPath.Equals(@"C:\Users\sarun\source\repos\UPS\TestProject\StructurlFailure\MusicLibrary\MusicLibrary.csproj"));
+            Assert.Contains(statuses, s => s.ActualPath.Equals(@"C:\Users\sarun\source\repos\UPS\TestProject\StructurlFailure\Helpers\MusicianLibrary\MusicianLibrary.csproj"));
+            Assert.Contains(statuses, s => s.ActualPath.Equals(@"C:\Users\sarun\source\repos\UPS\TestProject\StructurlFailure\DatabaseConnection\DatabaseConnection.csproj"));
         }
     }
 }
--- UPS/ProjectStructurizer.cs
+++ UPS/ProjectStructurizer.cs
@@ -6,14 +6,18 @@ namespace UPS
 {
     public class ProjectStructurizer
     {
-        public IEnumerable<ProjectStatus> GetProjectStatuses(string slnFile)
+        public IEnumerable<ProjectStatus> GetProjectStatuses(string slnFilePath)
         {
-            var fileContent = System.IO.File.ReadAllText(slnFile);
+            var fileContent = System.IO.File.ReadAllText(slnFilePath);
             var projectRegex = @"FAE04EC0-301F-11D3-BF4B-00C04F79EFBC.*?, ""(?<projectName>.*?)""";
             var regex = new Regex(projectRegex);
             var matches = regex.Matches(fileContent)
                 .Select(m => m.Groups["projectName"].Value)
-                .Select(s => new ProjectStatus { ProjectName = GetFileName(s) });
+                .Select(s => new ProjectStatus
+                {
+                    ProjectName = GetFileName(s),
+                    ActualPath = PathBasedOnSln(slnFilePath, s)
+                });
 
             return matches;
         }
@@ -22,6 +26,20 @@ namespace UPS
         {
             var parts = path.Split('\\');
             return parts.Last();
+        }
+
+        private string PathBasedOnSln(string slnPath, string project)
+        {
+            var slnBaseDir = System.IO.Path.GetDirectoryName(slnPath);
+            var expectedPath = System.IO.Path.Combine(slnBaseDir, project);
+            if (System.IO.File.Exists(expectedPath))
+            {
+                return expectedPath;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
